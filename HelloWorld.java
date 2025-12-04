196884e4ddb138b9420bda16599826a50f71819e
gadithirupathi12 <gadithirupathi12@gmail.com>
2025-12-04 15:29:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, chintupatel");
      System.out.println("Hello, krishnareddysir");
-     System.out.println("Hello, gadithirupathi143"); 
+     System.out.println("Hello, gadithirupathi1431"); 
 
   }
 }
